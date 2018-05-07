a3c62d22d50db1d5056ea967e719c67cb3921763
rclark19 <37247111+rclark19@users.noreply.github.com>
2018-05-07 10:19:12 +0100

    Update 5. Extract data from MySQL.py

--- scripts/5. Extract data from MySQL.py
+++ scripts/5. Extract data from MySQL.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 24 12:24:23 2018
+# File: 1.Chromosome8 database
+# Version: v1.0 
+# Date: 6/5/2018 
+# Copyright: (c) Rachel Julie Clark, Birkbeck, University of London, 2018
+# Author: Rachel Julie Clark
 
-@author: Rachel
-"""
+#Description of Script: The following script exports data from the three tables within the Chromosome8 database into python list format.
 
 import pymysql
 
@@ -18,23 +19,23 @@ port   =
 cnx = pymysql.connect(host=dbhost, port=port, user=dbuser, passwd=dbpass, db=dbname)
 cursor = cnx.cursor(pymysql.cursors.DictCursor)
 
-# Collate Genbank Data   
+# Collate genbank table Data   
 with cnx.cursor() as  cursor:
     query1 =  "SELECT accession, gene, product, source FROM genbank;"
     cursor.execute(query1)
     genbank = cursor.fetchall()
 
-# Collate Sequence Data
+# Collate sequence table data
 
 with cnx.cursor() as cursor:
     query2 = "SELECT accession, sequence FROM sequence;"
     cursor.execute(query2)
     sequence = cursor.fetchall()
 
-# Collate Coding Sequence
+# Collate coding_regions table data
 with cnx.cursor() as cursor:
     query4 = " SELECT accession,  codon_start, positions FROM coding_regions;"
     cursor.execute(query4)
     coding_regions = cursor.fetchall()
 
-print('export completed')
+print('export completed')                          # confirm data export complete 
