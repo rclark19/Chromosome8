6d35380da9a9f5bb7ad6a4590cff5bfb0e57757f
Rachel Julie Clark <37247111+rclark19@users.noreply.github.com>
2018-05-07 20:45:45 +0100

    Delete 5a. Extracted genbank table data example.py

--- 5a. Extracted genbank table data example.py
+++
@@ -1,7 +0,0 @@
-Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:\Users\Invate\AppData\Local\lxss\root\biocompt\RUNNING SCRIPT - Copy (2) - Copy\5. Extract data from MySQL.py 
-(('AB000381.1', 'GPAA1', 'GPI-anchored molecule-like protein', '8q24.3'), ('AB002137.1', 'GPAA1', 'glycosylphosphatidylinositol anchor attachment 1', '8q24.3'), ('AB007156.1', 'GPAA1', 'ribosomal protein S20', '8q21'), ('AB007168.1', 'GPAA1', 'ribosomal protein S20', '8q24.3'), ('AB013139.1', 'GPAA1', 'ribosomal protein S20', '8p11.2'), ('AB017267.1', 'GPAA1', 'ribosomal protein L8', '8q24.3'), ('AB022785.1', 'GPAA1', 'ribosomal protein L8', '8q22.3-q24.1'), ('AB026546.1', 'GPAA1', 'ribosomal protein L8', '8q11.2-q13'), ('AB032013.2', 'GPAA1', 'ribosomal protein L8', '8q11'), ('AB036532.2', 'GPAA1', 'NBS1', '8q21.1-q21.2'), ('AB051619.1', 'GPAA1', 'glycosylphosphatidylinositol anchor attachment 1', '8q24.3'), ('AB052953.1', 'GPAA1', 'RECQL4 helicase', '8q11.2-q13'), ('AB061209.1', 'GPAA1', 'potassium channel Kv8.1', '8q22.3'))
-export completed
->>> 
